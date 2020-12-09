a8d7c3bb204f206cd7f6afd9bcb44f3c735ecb61
felix <25915239+f3l1xss@users.noreply.github.com>
2020-12-10 01:49:08 +0700

    Added missing @Table name annotation

--- src/main/java/com/felixstanley/makanmoerahcalculateaveragestarjob/entity/RestaurantReviewSummary.java
+++ src/main/java/com/felixstanley/makanmoerahcalculateaveragestarjob/entity/RestaurantReviewSummary.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 /**
  * @author Felix
  */
 @Entity
+@Table(name = "restaurant_review_summary")
 @Data
 public class RestaurantReviewSummary implements Serializable {
 
